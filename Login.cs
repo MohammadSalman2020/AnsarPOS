cb42bf4f68c4c3a987a27c0156600f5bb9e73ce7
Muhammad Salman <mssallu546789@gmail.com>
2023-01-31 01:43:30 +0500

    Design Login,Signup,SplashScreen Form

---
+++ Login.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace AnsarPOS
+{
+    public partial class Login : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+   (
+       int nLeftRect,     // x-coordinate of upper-left corner
+       int nTopRect,      // y-coordinate of upper-left corner
+       int nRightRect,    // x-coordinate of lower-right corner
+       int nBottomRect,   // y-coordinate of lower-right corner
+       int nWidthEllipse, // width of ellipse
+       int nHeightEllipse // height of ellipse
+   );
+
+        public Login()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+        
+            SignUp signUp = new SignUp();
+            signUp.Show();
+            this.Hide();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
